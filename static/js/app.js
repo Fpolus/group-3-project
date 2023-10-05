bfea66a0e56c088c66164bd369ed0c1ecec783c3
AdalbertPayan <adalbertpayan@gmail.com>
2023-10-04 19:52:49 -0700

    final

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,24 @@
-function scheduleChange() {
-    var team = document.getElementById("team-selector");
-    team = team.options[team.selectedIndex].text;
-    var week = document.getElementById("week-selector");
-    week = week.options[week.selectedIndex].text;
-    d3.json(`https://api.sportsdata.io/v3/nfl/scores/json/Schedules/2023?key=12592b33cc6c450bb245e1917ffc3b44`).then((data) => {
-    console.log(data);
-    var weekData = data.filter(d => d.Week === parseInt(week));
-    console.log(weekData);
+function scheduleChange(week) {
     console.log(week);
-    })
-}
+    d3.json("https://api.sportsdata.io/v3/nfl/scores/json/Schedules/2023?key=12592b33cc6c450bb245e1917ffc3b44").then((data) => {
+
+    console.log(data);
+    let resultArray = data.filter(sampleObj => "week" + sampleObj.Week == week);
+    console.log(resultArray);
+
+    //reference table
+    let schedule = d3.select("tbody");
+
+    resultArray.forEach((schedulerow) => {
+    
+        let row = schedule.append("tr");
+        row.append("td").append("td").text(schedulerow.HomeTeam);
+        row.append("td").append("td").text(schedulerow.AwayTeam);
+        row.append("td").append("td").text(schedulerow.DateTime.split("T")[0]);
+        row.append("td").append("td").text(schedulerow.DateTime.split("T")[1]);
 
 
-
-
-
+    
+    });
+})
+    }
